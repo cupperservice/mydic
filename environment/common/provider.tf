ab267bb98a08afe5c3236243ed08324405fd3f4e
kawasima kazuhisa <cupper@mac.com>
2022-09-18 18:05:53 +0900

    add a circleci settings.

--- environment/common/provider.tf
+++ environment/common/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket    = "circleci-mydic"
+    region    = "ap-northeast-1"
+    profile   = "mydic"
+    key       = "terraform.tfstate"
+    encrypt   = true
+  }
+  
 }
 
 # Configure the AWS Provider
